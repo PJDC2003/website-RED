a74b9dc48d68da2be024e6e4a5607b2228122ad9
Pedro Correia <pedro.jorge.duarte.correia@tecnico.ulisboa.pt>
2023-08-15 04:09:55 +0100

    alt

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,3 @@
-function dropdownNavbar(){
-    document.getElementById('nav').classList.toggle('visible');
-
-    var reveals = document.querySelectorAll(".reveal_navbar");
-    for (var i = 0; i < reveals.length; i++) {
-      // Clears all the sub classes
-      navBarWipe(reveals[i]);
-      // Adds the right subclass
-      navBarPaint(reveals[i]);
-    }
-}
-
-
 window.addEventListener('scroll', () => {
   document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
 }, false);
